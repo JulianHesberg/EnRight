9a243b2b65e8f9865d9099d983efe42805ff592b
Abid <abdalo01@easv365.dk>
2025-03-06 13:42:41 +0100

    Init the cleaner file, setup with rabbitMq, basic funcitons, needs further implementation

--- backend/MailCleaner/Program.cs
+++ backend/MailCleaner/Program.cs
@@ -1,3 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-Console.WriteLine("Hello, World!");
+Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        // Register our MailCleanerWorker as a hosted service
+        services.AddHostedService<MailCleanerWorker>();
+    })
+    .Build()
+    .Run();
